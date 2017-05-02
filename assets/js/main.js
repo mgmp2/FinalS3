1ff29c50d091dbf8668a04be0e72b49cf19ec84d
Miriam Mendoza <miriammp1997@gmail.com>
2017-05-02 17:20:29 -0500

    add more color in header with scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 
 var lastScrollTop = 0;
+var a = document.getElementsByClassName("menu-link");
 window.addEventListener("load", function (e){
   e.preventDefault();
 })
@@ -11,10 +12,19 @@ window.addEventListener("scroll", function(){
   } else {
     document.getElementById("nav-header").classList.add("header-in");
     document.getElementById("nav-header").classList.add("solid");
+    document.getElementById("logo").classList.remove("logo");
+    document.getElementById("logo").classList.add("logo-2");
     document.getElementById("nav-header").style.opacity = 1;
+    for(var i = 0 ; i<a.length; i++){ a[i].style.color="black";}
+
     if(currentScroll<=3){
       document.getElementById("nav-header").classList.remove("solid");
       document.getElementById("nav-header").classList.remove("header-in");
+      document.getElementById("logo").classList.remove("logo-2");
+      document.getElementById("logo").classList.add("logo");
+      for(var i = 0 ; i<a.length; i++){ a[i].style.color="white";}
+
+
     }
   }
   lastScrollTop = currentScroll;
