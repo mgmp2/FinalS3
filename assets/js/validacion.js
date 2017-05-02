01b57d3b686eef079fd032ac292eb0a417886bda
Miriam Mendoza <miriammp1997@gmail.com>
2017-05-02 17:56:38 -0500

    function phone

--- assets/js/validacion.js
+++ assets/js/validacion.js
@@ -12,11 +12,18 @@ var expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 phone.addEventListener("click", function(){
   formMostrar.classList.remove("hidden-input");
 });
+phone.addEventListener("blur", validarphone);
 nom.addEventListener("blur", validarNombre);
 
 
 function validarNombre(){
   if(!expRegName.test(nom.value)){
     alert("Se escribe con mayuscula");
+  }
+}
+
+function validarphone(){
+  if(!expRegTelf.test(phone.value)){
+    alert("Se escribe sólo números");
   }
 }
